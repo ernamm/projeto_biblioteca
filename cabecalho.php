74f00ee22cd247b386d4e4015b206676e5b0efb7
lippekstro <fellipecastro13@hotmail.com>
2022-10-21 03:51:02 -0300

    mudanca na pagina sobre e ajuste do favicon

--- cabecalho.php
+++ cabecalho.php
@@ -15,7 +15,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bem Vindo</title>
-    <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../img/bd_logo_bgr.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/biblioteca.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Charmonman">
--- html/sobre.php
+++ html/sobre.php
@@ -5,13 +5,16 @@ require_once '../cabecalho.php';
 ?>
 
 <div class="conteudo">
+    <div style="display: flex; justify-content:center;">
+        <img src="../img/bd_logo_bgr.png" alt="" style="background-image: var(--gradient); border-radius: 10000px; width: 30%;">
+    </div>
     <div id="historia">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Architecto et quisquam modi autem eligendi corporis ratione facere nihil,
             veniam officia quia omnis deleniti impedit corrupti obcaecati cupiditate, mollitia sed est?</p>
     </div>
 
-    <div id="valores">
+    <!--<div id="valores">
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Nihil aspernatur quisquam, facilis sint odio obcaecati beatae atque odit consectetur, iure ex!
             Cumque alias voluptatibus iure mollitia explicabo molestias ullam assumenda.</p>
@@ -27,7 +30,7 @@ require_once '../cabecalho.php';
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Nihil aspernatur quisquam, facilis sint odio obcaecati beatae atque odit consectetur, iure ex!
             Cumque alias voluptatibus iure mollitia explicabo molestias ullam assumenda.</p>
-    </div>
+    </div>-->
 
 </div>
 
